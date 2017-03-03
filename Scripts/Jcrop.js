995ddea51d2ebe1a7401123f11baf098d3ad3d98
leon <leonliao.iisi@gmail.com>
2017-03-03 14:21:32 +0800

    Jcrop test with upload image
    測試圖片上傳並誆選功能

---
+++ Scripts/Jcrop.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function () {
+
+	function setJcrop(){
+		$("#jcrop_target").Jcrop({  
+		  onChange: showCoords,  
+		  onSelect: showCoords  
+		});
+	}
+
+
+	function showCoords(c)
+  	{
+	    $('#x1').val(c.x);
+	    $('#y1').val(c.y);
+	    $('#x2').val(c.x2);
+	    $('#y2').val(c.y2);
+	    $('#w').val(c.w);
+	    $('#h').val(c.h);
+  	};
+
+
+  	$("#imgUploadBtn").change(function(){
+		if (this.files && this.files[0]) {
+			var reader = new FileReader();	
+			reader.onload = function (e) {
+				$("#jcrop_target").attr('src', e.target.result);
+				setJcrop();
+			}			
+			reader.readAsDataURL(this.files[0]);
+		}
+	});
+});
